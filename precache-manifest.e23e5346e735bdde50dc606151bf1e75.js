fdd6141022648c8f55e8d0c691787b4af6111c36
pksatyam777 <pksatyam777@gmail.com>
2021-03-20 20:11:59 +0530

    Updates

---
+++ precache-manifest.e23e5346e735bdde50dc606151bf1e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c19f2627fed935ecbc16ff77bad96d",
+    "url": "/React-Youtube-App/index.html"
+  },
+  {
+    "revision": "0eea8529f0a1d7c4489b",
+    "url": "/React-Youtube-App/static/css/main.b7e903a4.chunk.css"
+  },
+  {
+    "revision": "1b41a83f67740042f53d",
+    "url": "/React-Youtube-App/static/js/2.2b093d78.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Youtube-App/static/js/2.2b093d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eea8529f0a1d7c4489b",
+    "url": "/React-Youtube-App/static/js/main.c9683184.chunk.js"
+  },
+  {
+    "revision": "0e1059332d159ead6403",
+    "url": "/React-Youtube-App/static/js/runtime-main.b2ea5edd.js"
+  }
+]);
